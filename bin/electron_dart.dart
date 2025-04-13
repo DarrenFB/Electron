c3c1fe20634fc299bdc39f5e8de37b871fe50001
Darren Fobissie Blese <dfobissieblese@gmail.com>
2025-04-13 16:33:56 -0400

    Initial commit, started program.

---
+++ bin/electron_dart.dart
@@ -0,0 +1,5 @@
+import 'package:electron_dart/electron.dart' as electron_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${electron_dart.calculate()}!');
+}
---
+++ test/electron_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:electron_dart/electron.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
